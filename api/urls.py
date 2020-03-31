08918e16544858a5f6a797b74b69c1dacb4f052f
adilet.nurlanov92@gmail.com <adilet.nurlanov92@gmail.com>
2020-03-31 11:28:49 +0600

    jwt settings

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,17 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('userapi/', include('userapi.urls')),
+
+    #   jwt
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
 ]
