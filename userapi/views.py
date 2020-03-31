cdb4889e59fa0dc895c18af9a52332b63a1dcb1a
adilet.nurlanov92@gmail.com <adilet.nurlanov92@gmail.com>
2020-03-31 12:14:39 +0600

    permissions added

--- userapi/views.py
+++ userapi/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from rest_framework.permissions import AllowAny
+from userapi.permissions import IsLoggedInUserOrAdmin, IsAdminUser
+
 from userapi.models import User
 from userapi.serializers import UserSerializer
 
@@ -8,3 +11,13 @@ from userapi.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'list' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
